069b33481c41510b5673a9400083768d12ad0b19
MarckoAbhi <150103446+MarckoAbhi@users.noreply.github.com>
2023-11-10 11:30:10 +0530

    Create sum_element.js

---
+++ sum_element.js
@@ -0,0 +1,15 @@
+function computeSum(array) {
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    return sum;
+}
+
+// Example usage:
+const numbers = [1, 2, 3, 4, 5];
+const result = computeSum(numbers);
+
+console.log("The sum of the elements is: " + result);
