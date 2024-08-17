ccbdcf068502351d2c83da0d1a322f200973ef6f
MarckoAbhi <150103446+MarckoAbhi@users.noreply.github.com>
2024-08-17 13:14:37 +0530

    Add files via upload

---
+++ JavaScript/hello.js
@@ -0,0 +1,41 @@
+const pi=3.14;
+
+console.log(pi);
+
+let name='Spider Man';
+let age='24';
+console.log(age);
+console.log(name);
+
+let option=true;
+let height;
+
+console.log(option);
+console.log(height);
+
+
+let shoping=[];
+shoping=['pen', 'laptop', 'usb'];
+
+console.log(shoping);
+
+let shoping1=[];
+shoping1=[1,2,3,4,5,6];
+
+console.log(shoping1);
+console.log(shoping1[1]+ shoping1[3]);
+shoping1.push(6);
+shoping1.pop(5);
+console.log(shoping1);
+
+
+let pen={
+    type:'ballpoint',
+    color:'blue',
+    cost:15
+
+};
+
+console.log(pen.type);
+console.log(pen.cost);
+console.log(pen.color);
